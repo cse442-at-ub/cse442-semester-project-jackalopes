7f14c070f27454e4f9e9b79bdb5bbc2fbf53a6a1
Jess <jrgrogan@buffalo.edu>
2020-02-26 15:10:20 -0500

    Edit Profile
    Screen done, functionality not up to par yet

--- roommate-frontend/navigation/BottomTabNavigator.js
+++ roommate-frontend/navigation/BottomTabNavigator.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
+import EditProfileScreen from '../screens/EditProfileScreen'
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
@@ -31,6 +32,14 @@ export default function BottomTabNavigator({ navigation, route }) {
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
         }}
       />
+      <BottomTab.Screen
+        name="Edit Profile"
+        component={EditProfileScreen}
+        options={{
+          title: 'Edit Profile',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+        }}
+      />
     </BottomTab.Navigator>
   );
 }
@@ -43,5 +52,7 @@ function getHeaderTitle(route) {
       return 'How to get started';
     case 'Links':
       return 'Links to learn more';
+    case 'Edit Profile':
+      return 'Edit Profile';
   }
 }
