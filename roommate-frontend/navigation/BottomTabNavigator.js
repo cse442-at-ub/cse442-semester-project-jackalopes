a984d7f4517cf37d1eb43d14b9cc7b1b76f62988
Peter Gottesman <peter@petergottesman.com>
2020-02-27 00:54:20 -0500

    Remove unused screens from app

--- roommate-frontend/navigation/BottomTabNavigator.js
+++ roommate-frontend/navigation/BottomTabNavigator.js
@@ -9,7 +9,7 @@ import EditProfileScreen from '../screens/EditProfileScreen';
 import ChatListScreen from '../screens/ChatListScreen';
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = 'Account';
 
 export default function BottomTabNavigator({ navigation, route }) {
   // Set the header title on the parent stack navigator depending on the
@@ -19,22 +19,6 @@ export default function BottomTabNavigator({ navigation, route }) {
 
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
-      <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
-        options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
-        }}
-      />
       <BottomTab.Screen
         name="Account"
           component={SettingsScreen}
@@ -67,10 +51,6 @@ function getHeaderTitle(route) {
   const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
     case 'Account':
       return 'Settings';
     case 'Edit Profile':
