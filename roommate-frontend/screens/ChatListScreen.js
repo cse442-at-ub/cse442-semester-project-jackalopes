424468b74f1393830aae5c422cf0e72caf930214
Peter Gottesman <peter@petergottesman.com>
2020-02-26 21:11:18 -0500

    Completed basic chat selection screen with sample data

--- roommate-frontend/screens/ChatListScreen.js
+++ roommate-frontend/screens/ChatListScreen.js
@@ -4,35 +4,80 @@ import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
-export default function LinksScreen() {
+// UI Elements
+export default function ChatListScreen({navigation}) {
+  const matches = [
+    {
+      photo: require('../assets/images/aaa.gif'),
+      name: "Peter",
+      last_message: "Wow please be my roommate",
+      id: 1,
+    },
+    {
+      photo: require('../assets/images/aaa.gif'),
+      name: "Tristan",
+      last_message: "Stay as far away from me as physically possible",
+      id: 2,
+    },
+    {
+      photo: require('../assets/images/aaa.gif'),
+      name: "Jess",
+      last_message: null,
+      id: 3,
+    },
+    {
+      photo: require('../assets/images/aaa.gif'),
+      name: "Hasan",
+      last_message: "You are such an amazing person",
+      id: 4,
+    },
+    {
+      photo: require('../assets/images/aaa.gif'),
+      name: "Judy",
+      last_message: "The aliens are talking to me please don't let them take me",
+      id: 5,
+    }];
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <OptionButton
-        photo="md-school"
-        name="Peter Gottesman"
-        last_message="Hey! Wanna live on my couch and pay half my rent?"
-        onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-      />
+      <ScrollView horizontal={true} style={styles.matchesBarContainer}>
+        {matches.map( (match) => !match.last_message &&  Match(match))}
+      </ScrollView>
+      {matches.map( (match) => match.last_message &&  ChatButton(match))}
     </ScrollView>
   );
-}
 
-function OptionButton({ photo, name, last_message, onPress, isLastOption }) {
-  return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionPhotoContainer}>
-          <Image style = {styles.optionPhoto}
-            source={require('../assets/images/robot-prod.png')}
+  function Match(match) {
+    return (
+      <RectButton key={match.id} style={styles.matchContainer}
+            onPress={() => navigation.navigate('Chat', {match: match})}>
+        <View style={styles.matchPhotoContainer}>
+          <Image style = {styles.matchPhoto}
+                 source={match.photo}
           />
         </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{name}</Text>
-          <Text style={styles.messageText}>{last_message}</Text>
+        <Text style={styles.matchName}>{match.name}</Text>
+      </RectButton>
+    );
+  }
+
+  function ChatButton(match) {
+    return (
+      <RectButton key={match.id} style={styles.match}
+                  onPress={() => navigation.navigate('Chat', {match: match})}>
+        <View style={styles.matchPhotoContainer}>
+          <Image style = {styles.matchPhoto}
+                 source={match.photo}
+          />
         </View>
-      </View>
-    </RectButton>
-  );
+        <View style={styles.matchTextContainer}>
+          <Text style={styles.matchNameChat}>{match.name}</Text>
+          <Text numberOfLines={1} style={[styles.messageText, {flexDirection: 'column'}]}>
+            {match.last_message}
+          </Text>
+        </View>
+      </RectButton>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -43,37 +88,54 @@ const styles = StyleSheet.create({
   contentContainer: {
     paddingTop: 15,
   },
-  optionPhotoContainer: {
-    width: 32,
-    height: 32,
-    marginRight: 12,
-  },
-  optionPhoto: {
+  matchPhoto: {
+    flex: 1,
     width: null,
     height: null,
-    flex: 1,
     resizeMode: 'contain',
   },
-  option: {
+  match: {
     backgroundColor: '#fdfdfd',
     paddingHorizontal: 15,
     paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
+    flexDirection: 'row',
   },
-  lastOption: {
+  matchTextContainer: {
+    flex: 1,
+  },
+  lastMatch: {
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
-  optionText: {
+  matchContainer: {
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  matchPhotoContainer: {
+    width: 48,
+    height: 48,
+    borderRadius: 50,
+    marginHorizontal: 10,
+    overflow: 'hidden',
+  },
+  matchName: {
     fontSize: 15,
+    marginTop: 1,
+  },
+  matchNameChat: {
+    fontSize: 17,
     alignSelf: 'flex-start',
     marginTop: 1,
   },
   messageText: {
-    fontSize: 11,
-    color: '#aaaaaa',
+    fontSize: 12,
+    color: '#8a8a8a',
     marginTop: 1,
   },
-
+  matchesBarContainer: {
+    backgroundColor: '#fefefe',
+    flexDirection: 'row',
+    borderColor: '#fafafa',
+    borderBottomWidth: 2,
+    padding: 3,
+  },
 });
