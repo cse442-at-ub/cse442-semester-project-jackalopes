a6e689ae4035a49911a6394fc73a6edd246efd7f
Tristan Wiley <tristan@tristanwiley.com>
2020-04-08 16:56:10 -0400

    Add API route and database for liking/disliking users, #41

--- roommate-backend/roommatefinder/roommatefinder/serializers.py
+++ roommate-backend/roommatefinder/roommatefinder/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class CurrentUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'picture_url')
+        fields = ('id', 'username', 'first_name', 'last_name', 'picture_url', 'likes')
