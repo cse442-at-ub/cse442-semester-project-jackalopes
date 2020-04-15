c5a391a31fe7dad7068212d3bf1f7add00563da0
Tristan Wiley <tristan@tristanwiley.com>
2020-04-14 21:21:56 -0400

    Add filter values to User model in Django backend #67

--- roommate-backend/roommatefinder/roommatefinder/models.py
+++ roommate-backend/roommatefinder/roommatefinder/models.py
@@ -2,9 +2,25 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import URLValidator
 from django.db import models
 
+gender_types = (
+    ('AN', 'Any'),
+    ('FE', 'Female'),
+    ('MA', 'Male'),
+    ('OT', 'Other')
+)
+
 
 class User(AbstractUser):
     picture_url = models.TextField(validators=[URLValidator(
     )], default="http://www-student.cse.buffalo.edu/CSE442-542/2020-Spring/cse-442o/cse442-semester-project-jackalopes/roommate-backend/roommatefinder/roommatefinder/assets/default-profile.png")
-    likes = models.ManyToManyField("User", related_name="user_likes+", blank=True)
-    dislikes = models.ManyToManyField("User", related_name="user_dislikes+", blank=True)
+    likes = models.ManyToManyField(
+        "User", related_name="user_likes+", blank=True)
+    dislikes = models.ManyToManyField(
+        "User", related_name="user_dislikes+", blank=True)
+
+    show_profile = models.BooleanField(default=True)
+    animal_friendly = models.BooleanField(default=True)
+    gender = models.CharField(
+        max_length=2, blank=False, null=False, default='AN', choices=gender_types)
+    max_age = models.IntegerField(range(18, 120), default=30)
+    max_distance = models.IntegerField(range(0, 100), default=30)
